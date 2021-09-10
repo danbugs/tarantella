68ffcb084354a9487c446e9aa0cdaa7761c104e6
danbugs <danilochiarlone@gmail.com>
2021-09-10 14:53:04 -0300

    tapm build now shows all that is printed to stderr and stdout (does not hide warnings anymore)

--- src/build.rs
+++ src/build.rs
@@ -1,18 +1,16 @@
 use crate::utils;
 use failure::Context;
-use std::str;
 
 pub fn build() -> Result<(), Context<String>> {
-    utils::check_for_command("make", "tapm depends on make — make sure you have got it installed.")?;
+    utils::check_for_command(
+        "make",
+        "tapm depends on make — make sure you have got it installed.",
+    )?;
     utils::check_for_command("emcc", "tapm depends on emcc — make sure you have got Emscripten installed. For instructions, visit: https://emscripten.org/docs/getting_started/downloads.html")?;
     utils::check_for_path("Makefile", "Makefile is missing")?;
     let build_dir = utils::check_for_toml_field("build_dir")?;
     utils::check_for_path(&build_dir, &format!("{} folder is missing", &build_dir))?;
-    let output = utils::run_command("emmake make", "tapm build failed")?;
-    let stderr = str::from_utf8(&output.stderr).unwrap();
-    if stderr.contains("emcc") { // we only care about an error from `emcc` — make prints to stderr even on success
-        return Err(Context::from(stderr.to_string()));
-    }
-    info!("{}", &format!("Created new build at {}/", &build_dir));
+    let mut child = utils::spawn_command("emmake make", "tapm build failed")?;
+    child.wait().unwrap();
     Ok(())
 }
